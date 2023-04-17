2021e448615939a8959fe6388d6f5c909e675fa1
suteetoe <sutee.toe@gmail.com>
2023-04-17 10:55:39 +0700

    try update app dedepos

--- lib/main_dedepos.dart
+++ lib/main_dedepos.dart
@@ -1,8 +1,9 @@
+import 'package:dedepos/main_app.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'flavors.dart';
 
 void main() {
   F.appFlavor = Flavor.DEDEPOS;
-  runApp(App());
+  mainApp();
 }
