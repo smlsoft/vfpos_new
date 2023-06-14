4476558dd24873ab8e5481b847222d7420ee644f
suteetoe <sutee.toe@gmail.com>
2023-06-14 13:51:18 +0700

    Update pos_screen.dart

--- lib/main_smlsuperpos.dart
+++ lib/main_smlsuperpos.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+import 'app/app_view.dart';
+import 'bootstrap.dart';
+import 'features/pos/presentation/screens/pos_secondary_screen.dart';
 import 'flavors.dart';
 
-void main() {
+void main() async {
   F.appFlavor = Flavor.SMLSUPERPOS;
-  runApp(const App());
+  initializeEnvironmentConfig();
+  WidgetsFlutterBinding.ensureInitialized();
+  Intl.defaultLocale = "th";
+  initializeDateFormatting();
+  await initializeApp();
+  runApp((isCustomerDisplayScreen()) ? const PosSecondaryScreen() : App());
 }
